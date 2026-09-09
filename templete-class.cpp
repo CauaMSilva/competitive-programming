c7d23ce92336a61d991cd3ca3cbc1063224063de
CauaMSilva <cauamaciel0107@gmail.com>
2026-09-09 16:22:22 -0300

    feat: add templete for leetcode problems

---
+++ templete-class.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f first
+#define s second
+#define dbg(x) cout << #x << " = " << x << endl;
+
+class Solution {
+public:
+    
+};
